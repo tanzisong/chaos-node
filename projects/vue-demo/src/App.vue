a6ab164d491e5ac66f86445e4a52fe57a2edfc47
tanzisong <tanzisong920@163.com>
2021-05-08 00:35:42 +0800

    feat(vue3-study): 使用vue3一些特性做一些小demo

--- projects/vue-demo/src/App.vue
+++ projects/vue-demo/src/App.vue
@@ -1,10 +1,39 @@
-<template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
-</template>
+<!--<template>-->
+<!--  <div id="nav">-->
+<!--    <component :is="component"></component>-->
+<!--    <router-link to="/">Home</router-link> |-->
+<!--    <router-link to="/about">About</router-link>-->
+<!--  </div>-->
+<!--  <router-view />-->
+<!--</template>-->
+
+<script lang="ts">
+import Vue, { defineComponent, resolveComponent, h, resolveDirective, getCurrentInstance } from 'vue';
+export default defineComponent({
+  setup(this, props, context) {
+    const A = resolveComponent('test-a');
+    console.info(h(A));
+    console.info(resolveDirective('highlight'));
+    console.info(this, props, context);
+    console.info('getCurrentInstance', getCurrentInstance());
+    const {
+      appContext: {
+        config: {
+          globalProperties: { foo },
+        },
+      },
+    } = getCurrentInstance()!;
+    console.info('foo', foo);
+    return () => h('div', {}, [h('router-view'), h(A)]);
+    // return {
+    //   component: 'test-a',
+    // };
+  },
+  mounted() {
+    console.info('全局变量', (this as any).foo);
+  },
+});
+</script>
 
 <style>
 #app {
---
+++ projects/vue-demo/src/components/a.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="hello">i am Iron man => 自定义的全局变量-> {{ foo }}</div>
+</template>
